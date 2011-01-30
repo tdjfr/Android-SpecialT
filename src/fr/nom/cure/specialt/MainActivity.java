6d2cf410205f5fbd27f2c95ca9eb3014d0ca49ac
david <david@lenny32.(none)>
2011-01-30 22:29:14 +0100

    Base UI defined

--- src/fr/nom/cure/specialt/MainActivity.java
+++ src/fr/nom/cure/specialt/MainActivity.java
@@ -5,20 +5,48 @@
 
 package fr.nom.cure.specialt;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 /**
  *
  * @author david
  */
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
+
+    String[] listTea={"Thé vert Japonais",
+                        "Thé Vert Chinois"};
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        // ToDo add your GUI initialization code here        
+        // ToDo add your GUI initialization code here
+        setContentView(R.layout.main);
+        setListAdapter(new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                listTea));
     }
 
+    // Manage menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.xml.menu, menu);
+        return true;
+}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.save:
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
--- src/fr/nom/cure/specialt/R.java
+++ src/fr/nom/cure/specialt/R.java
@@ -10,10 +10,17 @@ package fr.nom.cure.specialt;
 public final class R {
     public static final class attr {
     }
+    public static final class id {
+        public static final int save=0x7f050000;
+    }
     public static final class layout {
         public static final int main=0x7f020000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
+    }
+    public static final class xml {
+        public static final int menu=0x7f030000;
+        public static final int tea=0x7f030001;
     }
 }
